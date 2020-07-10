acc28a5382d0c35657cded153b8835925528ee0a
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-07-09 19:57:01 -0700

    Updates

---
+++ precache-manifest.b1dd643509aaf5d0a6fcf956c3a1159f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12b7fa7b2b43a4b55a87ec997847125",
+    "url": "/job-listing-challenge/index.html"
+  },
+  {
+    "revision": "2f7f4f2115fe4dfb1404",
+    "url": "/job-listing-challenge/static/js/2.9ba9e6d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/job-listing-challenge/static/js/2.9ba9e6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae460c63826770ad1d3",
+    "url": "/job-listing-challenge/static/js/main.8a4560c1.chunk.js"
+  },
+  {
+    "revision": "ef29ed6fc2a490ef5d1f",
+    "url": "/job-listing-challenge/static/js/runtime-main.0832860d.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/job-listing-challenge/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/job-listing-challenge/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/job-listing-challenge/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/job-listing-challenge/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/job-listing-challenge/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/job-listing-challenge/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/job-listing-challenge/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/job-listing-challenge/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/job-listing-challenge/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/job-listing-challenge/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/job-listing-challenge/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/job-listing-challenge/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/job-listing-challenge/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
